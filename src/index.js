c39cefa974cf501adc8a1b5351983bc71287ad0d
Luan Andrade <luansandrade92@hotmail.com>
2020-07-26 15:58:05 -0300

    Cria maquina do tempo para navegar o longo das jogadas
    Função jumpTo

--- src/index.js
+++ src/index.js
@@ -52,12 +52,13 @@ class Game extends React.Component {
         squares: Array(9).fill(null),
       }],
       xIsNext: true,
+      stepNumber: 0,
     };
   }
 
   handleClick(i) {
-    const history = this.state.history;
-    const current = history[history.length -1];
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[this.state.stepNumber];
     const squares = current.squares.slice();
 
     if (calculateWinner(squares) || squares[i]) {
@@ -70,13 +71,21 @@ class Game extends React.Component {
         squares: squares
       }]),
       xIsNext: !this.state.xIsNext,
+      stepNumber: history.length,
+    });
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0,
     });
   }
 
   render() {
 
     const history = this.state.history;
-    const current = history[history.length -1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
 
     const moves = history.map((step, move) => {
@@ -84,7 +93,7 @@ class Game extends React.Component {
         'Go to move #' + move :
         'Go to game start';
       return (
-        <li>
+        <li key={move}>
           <button onClick={()=> this.jumpTo(move)}>{desc}</button>
         </li>
       );
